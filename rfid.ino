8ec9fc1091ded4bf2e1827e615ad70a9b383f516
NGUYỄN VĂN QUÂN <57479794+anhquantr123@users.noreply.github.com>
2021-10-03 17:31:20 +0700

    Update rfid.ino
    change

--- rfid.ino
+++ rfid.ino
@@ -3,12 +3,18 @@
 #define RST_PIN 9
 #define SS_PIN 10
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522 mfrc522(SS_PIN, RST_PIN);       
+unsigned long uidDec, uidDecTemp; 
+byte bCounter, readBit;
+unsigned long ticketNumber;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
+  Serial.println("await card, please insert card....");
 }
 
 void loop() {
@@ -24,5 +30,22 @@ void loop() {
   {
     return;
   }
+  
+  uidDec = 0;
+  Serial.println("==> Dang cho quyet the <===");
+  Serial.println("================================================");
+  
+  Serial.println("This is UID Card : ");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    uidDecTemp = mfrc522.uid.uidByte[i];
+    uidDec = uidDec*256+uidDecTemp;
+  } 
+  Serial.print("            [");
+  Serial.print(uidDec);
+  Serial.println("]");
+  Serial.println("================================================");
+
+  
+  
   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
